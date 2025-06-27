75b48bff52fea2efc91366e349bc656e1929da14
pivnati <natanmuleta911@gmail.com>
2025-06-26 17:32:48 -0700

    Finish the Herosection

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,10 @@
+import HeroSection from "./components/HeroSection";
 export default function Home() {
   return (
-   
-      <main className="flex min-h-screen flex-col bg-[#121212]">
-      </main>
+    <main className="flex min-h-screen flex-col bg-[#121212]">
+      <div className="container mx-auto px-12 py-4">
+        <HeroSection />
+      </div>
+    </main>
   );
 }
