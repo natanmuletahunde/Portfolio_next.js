11d35fc145f8533ed1331853f4ed81d2fb618c38
pivnati <natanmuleta911@gmail.com>
2025-06-27 12:06:37 -0700

    make the path @ in to the ./

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,11 @@
 import Image from "next/image";
-import HeroSection from "@/components/HeroSection";
-import Navbar from "@/components/Navbar";
+import HeroSection from "./components/HeroSection";
+import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
-      <Navbar />
+      <Navbar />.
       <div className="container mx-auto mt-24 px-12 py-4">
         <HeroSection />
       </div>
