92a384b513cae604b4c8930717fe4a006a5fa8e1
pivnati <natanmuleta911@gmail.com>
2025-06-27 16:06:16 -0700

    install the npm intall freamer-motion react-animated-number

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -10,12 +10,11 @@ const HeroSection = () => {
         <div className="col-span-7 place-self-center text-center sm:text-left">
           <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
-              Hello, I'm{" "}
+              Hello, I'm Natan Muleta
             </span>
             <br></br>
             <TypeAnimation
               sequence={[
-                "Judy",
                 1000,
                 "Web Developer",
                 1000,
